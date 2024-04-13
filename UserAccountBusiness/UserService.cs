64a77d92aeb28680406f4cd999cde753565decb0
Aldrin Kyle <Aldrin Kyle@KYLE-BOOK19>
2024-04-13 10:27:40 +0800

    Add project files.

---
+++ UserAccountBusiness/UserService.cs
@@ -0,0 +1,16 @@
+﻿using UserAccountDataServices;
+
+namespace UserAccountBusiness
+{
+    public class UserService
+    {
+        public bool VerifyUser(string userName, string password)
+        {
+            UserDataService dataService = new UserDataService();
+            var result = dataService.GetUser( userName);
+
+            return result.userName != null ? true : false;
+            //return result.password != null ? true : false;
+        }
+    }
+}
---
+++ UserAccountDataServices/UserDataService.cs
@@ -0,0 +1,47 @@
+﻿
+
+
+
+using UserDataModel;
+
+namespace UserAccountDataServices
+{
+    public class UserDataService
+    {
+        List<User> dummyUser= new List<User>();
+
+        public UserDataService()
+        {
+            CreateDummyData();
+        }
+        private void CreateDummyData()
+        {
+            User user1 = new User();
+            user1.userName = "Karyle" ;
+            user1.password = "Dela Cruz";
+            User user2 = new User();
+            user2.userName = "Ellis" ;
+            user2.password = "Arciaga";
+            User user3 = new User();
+            user3.userName = "Joana" ;
+            user3.password = "Naval";
+
+            dummyUser.Add(user1);
+            dummyUser.Add(user2);
+            dummyUser.Add(user3);
+        }
+        public User GetUser(string userName)
+        {
+            User foundUser = new User();
+            
+            foreach (var dummy in dummyUser)
+            {
+                if (userName == dummy.userName)
+                {
+                    foundUser = dummy;
+                }
+            }
+            return foundUser;
+        }
+        }
+}
